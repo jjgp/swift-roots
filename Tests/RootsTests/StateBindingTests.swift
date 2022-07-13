e1ce2cf84449133310f09d9f5d56764a256d9529
Jason Prasad <jasongprasad@gmail.com>
2022-07-13 11:40:22 +0100

    Add twin state binding tests

--- Tests/RootsTests/StateBindingTests.swift
+++ Tests/RootsTests/StateBindingTests.swift
@@ -11,7 +11,7 @@ class StateBindingTests: XCTestCase {
         XCTAssertEqual(count, binding.wrappedState)
     }
 
-    func testMappedBinding() {
+    func testBindingInScope() {
         let pingPong = PingPong()
         let pingPongBinding = StateBinding(initialState: pingPong)
         let pingBinding = pingPongBinding.scope(\.ping)
@@ -24,5 +24,27 @@ class StateBindingTests: XCTestCase {
         let pingValues = pingSpy.values.map(\.count)
         XCTAssertEqual(pingPongPingValues, [0, 42, 21, 1337])
         XCTAssertEqual(pingValues, [0, 42, 21, 1337])
+    }
+
+    func testTwinBindingsInScope() {
+        let pingPong = PingPong()
+        let pingPongBinding = StateBinding(initialState: pingPong)
+        let pingBinding = pingPongBinding.scope(\.ping)
+        let twinPingBinding = pingPongBinding.scope(\.ping)
+        let pingPongSpy = PublisherSpy(pingPongBinding)
+        let pingSpy = PublisherSpy(pingBinding)
+        let twinPingSpy = PublisherSpy(twinPingBinding)
+        pingBinding.wrappedState.count = 42
+        twinPingBinding.wrappedState.count -= 42
+        pingPongBinding.wrappedState.ping.count = 21
+        twinPingBinding.wrappedState.count -= 21
+        pingBinding.wrappedState.count = 1337
+        twinPingBinding.wrappedState.count -= 1337
+        let pingPongPingValues = pingPongSpy.values.map(\.ping.count)
+        let pingValues = pingSpy.values.map(\.count)
+        let twinPingValues = twinPingSpy.values.map(\.count)
+        XCTAssertEqual(pingPongPingValues, [0, 42, 0, 21, 0, 1337, 0])
+        XCTAssertEqual(pingValues, [0, 42, 0, 21, 0, 1337, 0])
+        XCTAssertEqual(twinPingValues, [0, 42, 0, 21, 0, 1337, 0])
     }
 }
