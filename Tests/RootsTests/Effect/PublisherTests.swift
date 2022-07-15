0330be97c85c0ce44e582036f7fbc65270dde850
Jason Prasad <jasongprasad@gmail.com>
2022-07-15 18:51:05 +0100

    Add sink tests

--- Tests/RootsTests/Effect/PublisherTests.swift
+++ Tests/RootsTests/Effect/PublisherTests.swift
@@ -18,5 +18,24 @@ class PublisherEffectTests: XCTestCase {
         XCTAssertEqual(values, [0, 10, -90])
     }
 
-    func testPublisherOfEnvironmentEffect() {}
+    func testPublisherOfEnvironmentEffect() {
+        struct Environment {
+            let incrementValue = 10
+            let decrementValue = 100
+        }
+        let environment = Environment()
+        let store = Store(
+            initialState: Count(),
+            reducer: Count.reducer(state:action:),
+            effect: .publisher(of: environment) { transitionPublisher, environment in
+                transitionPublisher
+                    .filter { $0.action == .increment(environment.incrementValue) }
+                    .map { _ in .decrement(environment.decrementValue) }
+            }
+        )
+        let spy = PublisherSpy(store)
+        store.send(.increment(10))
+        let values = spy.values.map(\.count)
+        XCTAssertEqual(values, [0, 10, -90])
+    }
 }
--- Tests/RootsTests/Effect/SinkTests.swift
+++ Tests/RootsTests/Effect/SinkTests.swift
@@ -2,7 +2,53 @@ import Roots
 import XCTest
 
 class SinkEffectTests: XCTestCase {
-    func testSinkEffect() {}
+    func testSinkEffect() {
+        var value = 0
+        let store = Store(
+            initialState: Count(),
+            reducer: Count.reducer(state:action:),
+            effect: .sink { transitionPublisher in
+                transitionPublisher
+                    .compactMap {
+                        if case let .increment(value) = $0.action {
+                            return value
+                        } else {
+                            return nil
+                        }
+                    }
+                    .sink {
+                        value = $0
+                    }
+            }
+        )
+        store.send(.increment(10))
+        XCTAssertEqual(value, 10)
+    }
 
-    func testSinkOfEnvironmentEffect() {}
+    func testSinkOfEnvironmentEffect() {
+        struct Environment {
+            let multiplier = 2
+        }
+        let environment = Environment()
+        var value = 0
+        let store = Store(
+            initialState: Count(),
+            reducer: Count.reducer(state:action:),
+            effect: .sink(of: environment) { transitionPublisher, environment in
+                transitionPublisher
+                    .compactMap {
+                        if case let .increment(value) = $0.action {
+                            return environment.multiplier * value
+                        } else {
+                            return nil
+                        }
+                    }
+                    .sink {
+                        value = $0
+                    }
+            }
+        )
+        store.send(.increment(10))
+        XCTAssertEqual(value, 20)
+    }
 }
