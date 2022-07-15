7db15bd0bf2c9ab0eec967efeb2c1a4e8ba85fc5
Jason Prasad <jasongprasad@gmail.com>
2022-07-15 18:55:20 +0100

    Add subject tests

--- Tests/RootsTests/Effect/SubjectTests.swift
+++ Tests/RootsTests/Effect/SubjectTests.swift
@@ -21,6 +21,28 @@ class SubjectEffectTests: XCTestCase {
     }
 
     func testSubjectOfEnvironmentEffect() {
+        struct Environment {
+            let multiplier = 2
+        }
+        let environment = Environment()
+        let store = Store(
+            initialState: Count(),
+            reducer: Count.reducer(state:action:),
+            effect: .subject(of: environment) { _, action, send, environment in
+                if case let .increment(value) = action {
+                    send(.decrement(environment.multiplier * value))
+                }
+            }
+        )
+        let spy = PublisherSpy(store)
+        store.send(.increment(10))
+        store.send(.increment(20))
+        store.send(.increment(40))
+        let values = spy.values.map(\.count)
+        XCTAssertEqual(values, [0, 10, -10, 10, -30, 10, -70])
+    }
+
+    func testAsyncSubjectEffect() {
         let expect = expectation(description: "The value is decremented")
         let store = Store(
             initialState: Count(),
@@ -42,7 +64,29 @@ class SubjectEffectTests: XCTestCase {
         XCTAssertEqual(values, [0, 10, -90])
     }
 
-    func testAsyncSubjectEffect() {}
-
-    func testAsyncSubjectEffectOfEnvironment() {}
+    func testAsyncSubjectEffectOfEnvironment() {
+        struct Environment {
+            let multiplier = 2
+        }
+        let environment = Environment()
+        let expect = expectation(description: "The value is decremented")
+        let store = Store(
+            initialState: Count(),
+            reducer: Count.reducer(state:action:),
+            effect: .subject(of: environment) { _, action, send, environment in
+                if case .increment = action {
+                    await MainActor.run {
+                        send(.decrement(environment.multiplier * 100))
+                    }
+                } else if case .decrement = action {
+                    expect.fulfill()
+                }
+            }
+        )
+        let spy = PublisherSpy(store)
+        store.send(.increment(10))
+        wait(for: [expect], timeout: 1)
+        let values = spy.values.map(\.count)
+        XCTAssertEqual(values, [0, 10, -190])
+    }
 }
