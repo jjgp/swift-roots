f6e922af33d0e2a582b413a3e4b180ba584cd1a4
Jason Prasad <jasongprasad@gmail.com>
2022-07-16 11:06:35 +0100

    Add comments to sink tests

--- Tests/RootsTests/Effect/SinkTests.swift
+++ Tests/RootsTests/Effect/SinkTests.swift
@@ -3,6 +3,7 @@ import XCTest
 
 class SinkEffectTests: XCTestCase {
     func testSinkEffect() {
+        // Given a store with a sink effect that observes the incremented value
         var value = 0
         let store = Store(
             initialState: Count(),
@@ -21,11 +22,16 @@ class SinkEffectTests: XCTestCase {
                     }
             }
         )
+
+        // When sending an increment by a value
         store.send(.increment(10))
+
+        // Then the observed value should be equal
         XCTAssertEqual(value, 10)
     }
 
     func testSinkOfEnvironmentEffect() {
+        // Given a store with a sink effect and environment that observes the incremented value and multiplies it
         struct Environment {
             let multiplier = 2
         }
@@ -48,7 +54,11 @@ class SinkEffectTests: XCTestCase {
                     }
             }
         )
+
+        // When sending an increment by a value
         store.send(.increment(10))
+
+        // Then the observed value should be a multiple of the original value
         XCTAssertEqual(value, 20)
     }
 }
