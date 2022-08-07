3d6776b1d54727a45b5b247583092fa93dd036ac
Jason Prasad <jasongprasad@gmail.com>
2022-08-07 15:15:52 -0400

    Add combine middleware implementation

--- Sources/Roots/Middleware/CombineMiddleware.swift
+++ Sources/Roots/Middleware/CombineMiddleware.swift
@@ -1,3 +1,15 @@
-func combine<State, Action>(middlewares _: Middleware<State, Action>...) -> Middleware<State, Action> {
-    fatalError()
+func combine<State, Action>(middlewares: Middleware<State, Action>...) -> Middleware<State, Action> {
+    combine(middlewares: middlewares)
+}
+
+func combine<State, Action>(middlewares: [Middleware<State, Action>]) -> Middleware<State, Action> {
+    .init { store in
+        { next in
+            middlewares.reversed().reduce({ action in
+                next(action)
+            }) { dispatch, middleware in
+                middleware.createDispatch(store)(dispatch)
+            }
+        }
+    }
 }
