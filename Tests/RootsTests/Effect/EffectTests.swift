01dbbb102804936420188c3533d5c12c8b44d5f5
Jason Prasad <jasongprasad@gmail.com>
2022-08-08 09:20:20 -0400

    Update effect tests individual stores

--- Tests/RootsTests/Effect/EffectTests.swift
+++ Tests/RootsTests/Effect/EffectTests.swift
@@ -17,60 +17,60 @@ class EffectTests: XCTestCase {
 
 class EffectsOfStoreInScopeTests: XCTestCase {
     func testEffectsOfStoresInScope() {
-        // Given a store that is scoped to two individual stores having the same increment/decrement effect
-        let pingPongSUT = Store(initialState: Counts(), reducer: Counts.reducer(state:action:))
+        // Given a store that is scoped to the individual Count stores having the same increment/decrement effect
+        let countsStore = Store(initialState: Counts(), reducer: Counts.reducer(state:action:))
 
-        let pingSUT = pingPongSUT.scope(
+        let firstCountStore = countsStore.scope(
             to: \.first,
             reducer: Count.reducer(state:action:),
             middleware: apply(effects: .decrementByDoubleIncrementedValue())
         )
-        let pongSUT = pingPongSUT.scope(
+        let secondCountStore = countsStore.scope(
             to: \.second,
             reducer: Count.reducer(state:action:),
             middleware: apply(effects: .decrementByDoubleIncrementedValue())
         )
 
-        let pingPongSpy = PublisherSpy(pingPongSUT)
-        let pingSpy = PublisherSpy(pingSUT)
-        let pongSpy = PublisherSpy(pongSUT)
+        let countsSpy = PublisherSpy(countsStore)
+        let firstCountSpy = PublisherSpy(firstCountStore)
+        let secondCountSpy = PublisherSpy(secondCountStore)
 
         // When each scoped store increments the values with a set sequence...
-        pingSUT.send(.increment(10))
-        pongSUT.send(.increment(10))
-        pingSUT.send(.increment(20))
-        pongSUT.send(.increment(20))
-        pingSUT.send(.increment(40))
-        pongSUT.send(.increment(40))
-        // ...and the parent/global store sends an action to reset the state
-        pingPongSUT.send(creator: \.initialize)
+        firstCountStore.send(.increment(10))
+        secondCountStore.send(.increment(10))
+        firstCountStore.send(.increment(20))
+        secondCountStore.send(.increment(20))
+        firstCountStore.send(.increment(40))
+        secondCountStore.send(.increment(40))
+        // ...and the parent store sends an action to reset the state
+        countsStore.send(creator: \.initialize)
 
         // Then each store should emit values that are consistent with one another
-        let pingPongValues = pingPongSpy.values.map { "\($0.first.count), \($0.second.count)" }
-        let pingValues = pingSpy.values.map(\.count)
-        let pongValues = pongSpy.values.map(\.count)
-        // Note that the ping/pong values are interleaved
+        let countsValues = countsSpy.values.map { [$0.first.count, $0.second.count] }
+        let firstCountValues = firstCountSpy.values.map(\.count)
+        let secondCountValues = secondCountSpy.values.map(\.count)
+        // Note that the first/second values are interleaved
         XCTAssertEqual(
-            pingPongValues,
+            countsValues,
             [
-                "0, 0",
-                "10, 0",
-                "-10, 0",
-                "-10, 10",
-                "-10, -10",
-                "10, -10",
-                "-30, -10",
-                "-30, 10",
-                "-30, -30",
-                "10, -30",
-                "-70, -30",
-                "-70, 10",
-                "-70, -70",
-                "0, 0",
+                [0, 0],
+                [10, 0],
+                [-10, 0],
+                [-10, 10],
+                [-10, -10],
+                [10, -10],
+                [-30, -10],
+                [-30, 10],
+                [-30, -30],
+                [10, -30],
+                [-70, -30],
+                [-70, 10],
+                [-70, -70],
+                [0, 0],
             ]
         )
-        XCTAssertEqual(pingValues, [0, 10, -10, 10, -30, 10, -70, 0])
-        XCTAssertEqual(pongValues, [0, 10, -10, 10, -30, 10, -70, 0])
+        XCTAssertEqual(firstCountValues, [0, 10, -10, 10, -30, 10, -70, 0])
+        XCTAssertEqual(secondCountValues, [0, 10, -10, 10, -30, 10, -70, 0])
     }
 
     func testEffectsOfTwinStoresInScope() {
