34803d031cb211006836f48d2cd01d838440f2f4
Jason Prasad <jasongprasad@gmail.com>
2022-08-08 22:34:31 -0400

    Store Tests around scoped predicate

--- Sources/Roots/Middleware/AssertDispatch.swift
+++ Sources/Roots/Middleware/AssertDispatch.swift
@@ -1,1 +1,14 @@
-// TODO: Assertion of serial dispatch
+import Foundation
+
+public extension Middleware {
+    static func assertDispatch(on queue: DispatchQueue) -> Self {
+        .init { _ in
+            { next in
+                { action in
+                    dispatchPrecondition(condition: .onQueue(queue))
+                    next(action)
+                }
+            }
+        }
+    }
+}
