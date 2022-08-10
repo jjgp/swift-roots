d6611e84c09dad66fd37b792b138f64f4b4add9d
Jason Prasad <jasongprasad@gmail.com>
2022-08-10 12:26:12 -0400

    Add priority to async side effects

--- Sources/Roots/Thunk/AsyncThunk.swift
+++
@@ -1,9 +0,0 @@
-public struct AsyncThunk<State> {
-    let run: Run
-
-    public init(run: @escaping Run) {
-        self.run = run
-    }
-
-    public typealias Run = (Dispatch<Action>, () -> State) async -> Void
-}
--- Sources/Roots/Thunk/Thunk.swift
+++ Sources/Roots/Thunk/Thunk.swift
@@ -5,5 +5,15 @@ public struct Thunk<State> {
         self.run = run
     }
 
-    public typealias Run = (Dispatch<Action>, () -> State) -> Void
+    public typealias Run = (@escaping Dispatch<Action>, @escaping () -> State) -> Void
+}
+
+public extension Thunk {
+    init(priority: TaskPriority? = nil, run: @escaping (Dispatch<Action>, () -> State) async -> Void) {
+        self.run = { dispatch, getState in
+            Task(priority: priority) {
+                await run(dispatch, getState)
+            }
+        }
+    }
 }
