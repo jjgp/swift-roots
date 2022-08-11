175f90a78a73b750cf2bb71339c2eea6c4f5bc8b
Jason Prasad <jasongprasad@gmail.com>
2022-08-11 19:19:33 -0400

    Add test for OneAtATimeScheduler

---
+++ Tests/RootsTests/SendSchedulerTests.swift
@@ -0,0 +1,29 @@
+@testable import Roots
+import XCTest
+
+class OneAtATimeSendSchedulerTests: XCTestCase {
+    func testRecursiveSendingIsBuffered() {
+        // Given a OneAtATimeSendScheduler
+        let sendScheduler = OneAtATimeSendScheduler()
+
+        var actions = [String]()
+        var send: Dispatch<String>!
+        send = { action in
+            actions.append(action)
+            if action == "foo" {
+                sendScheduler.schedule(action: "bar", sendingTo: send)
+                sendScheduler.schedule(action: "baz", sendingTo: send)
+                XCTAssertEqual(sendScheduler.sendPendingBuffer.count, 2)
+            } else if action == "bar" {
+                XCTAssertEqual(sendScheduler.sendPendingBuffer.count, 1)
+            } else if action == "baz" {
+                XCTAssertEqual(sendScheduler.sendPendingBuffer.count, 0)
+            }
+        }
+
+        // When an action is sent that triggers recursive actions
+        // Then the other actions should be bufferred to allow for the others to complete
+        sendScheduler.schedule(action: "foo", sendingTo: send)
+        XCTAssertEqual(actions, ["foo", "bar", "baz"])
+    }
+}
