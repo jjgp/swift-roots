74d06aee165b35b5ccc4cb7e4450893708e1da8f
Jason Prasad <jasongprasad@gmail.com>
2022-08-12 21:45:04 -0400

    Use generics to build roots

--- Tests/RootsTests/RootsTests.swift
+++ Tests/RootsTests/RootsTests.swift
@@ -6,21 +6,42 @@ class RootsTests: XCTestCase {
     }
 }
 
-public protocol Root {}
-public struct Call: Root {}
-public struct Put: Root {}
-public struct Take: Root {}
+public protocol Root {
+    associatedtype Action
+}
 
-@resultBuilder
-public enum RootsBuilder {
-    static func buildBlock(_ roots: Root...) -> Root {
-        print(roots)
-        return Take()
+public struct Put<Action>: Root {
+    let action: Action
+
+    public init(_ action: Action) {
+        self.action = action
+    }
+
+    public init(_: (Action) -> Action) {
+        fatalError()
     }
 }
 
-@RootsBuilder func rootsBuilder() -> Root {
-    Take()
-    Put()
-    Call()
+public struct Take<Action>: Root {
+    public init(_: Action) where Action: Equatable {}
+
+    public init(_: (Action, Action) -> Bool) {}
+}
+
+public struct Roots<Action> {}
+
+@resultBuilder
+public enum RootsBuilder {
+    static func buildBlock<Action, R: Root>(_: R) -> Roots<Action> where R.Action == Action {
+        Roots()
+    }
+
+    static func buildBlock<Action, R0: Root, R1: Root>(_: R0, _: R1) -> Roots<Action> where R0.Action == Action, R1.Action == Action {
+        Roots()
+    }
+}
+
+@RootsBuilder func rootsBuilder() -> Roots<String> {
+    Take("foobar")
+    Put("foobar")
 }
