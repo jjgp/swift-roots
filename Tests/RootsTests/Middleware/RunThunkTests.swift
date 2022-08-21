66b00bbcd23f52343f555f8ed73fee8f0cff34c6
Jason Grant Prasad <jasongprasad@gmail.com>
2022-08-21 10:52:17 -0400

    Thunk Refactor (#16)
    * Add support to run thunk from store directly
    
    This circumvents the need to annotate a thunk as an Action that could be different than its templated Action
    
    * Refactor Thunk API

--- Tests/RootsTests/Middleware/RunThunkTests.swift
+++ Tests/RootsTests/Middleware/RunThunkTests.swift
@@ -4,11 +4,11 @@ import XCTest
 
 class RunThunkTests: XCTestCase {
     func testRunningOfAsyncThunk() {
-        let countStore = Store(initialState: Counts(), reducer: Counts.reducer(state:action:), middleware: RunThunk())
+        let countStore = Store(initialState: Counts(), reducer: Counts.reducer(state:action:))
         let countSpy = PublisherSpy(countStore)
 
         let expectation = expectation(description: "the dispatch is sent")
-        countStore.send(Counts.Thunk { dispatch, _ in
+        countStore.run(thunk: Counts.Thunk { dispatch, _ in
             await MainActor.run {
                 dispatch(Counts.Addition(to: \.first, by: 100))
                 expectation.fulfill()
